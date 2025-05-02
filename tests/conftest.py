ab4cc8258cf719c344a8346f17752a291bd07aa0
Rui Vieira <ruidevieira@googlemail.com>
2025-05-02 11:39:36 +0100

    chore: Add TLS configuration unit tests

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+"""Configuration file for the LMEval Llama Stack Provider test."""
+
+import sys
+from pathlib import Path
+
+module_path = Path(__file__).parent.parent
+sys.path.insert(0, str(module_path))
