153c1f0207b58e7253f94f18ef373aee509dc914
Christina Xu <chrxu@redhat.com>
2025-09-30 11:27:36 -0400

    chore: Bump LLS version to 0.2.23

--- src/llama_stack_provider_lmeval/provider.py
+++ src/llama_stack_provider_lmeval/provider.py
@@ -8,6 +8,7 @@ from llama_stack.providers.datatypes import (
 def get_provider_spec() -> ProviderSpec:
     return RemoteProviderSpec(
         api=Api.eval,
+           provider_type="remote::trustyai_lmeval",
         adapter_type="lmeval",
         pip_packages=["kubernetes"],
         config_class="llama_stack_provider_lmeval.config.LMEvalEvalProviderConfig",
